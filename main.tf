31cbbfbfc0f68e34868f3cd2ba097a38234bd3f2
sruthilaya <sruthilayagadila@gmail.com>
2023-12-20 15:25:02 +0530

    terraform

--- main.tf
+++ main.tf
@@ -1,7 +1,11 @@
 module "instances" {
-  for_each = var.component
-  source = "git::https://github.com/Gadilasruthilaya/tf-module-app.git"
+  for_each  = var.component
+  source    = "git::https://github.com/Gadilasruthilaya/tf-module-app.git"
   component = each.key
-  env= var.env
+  env       = var.env
 
 }
+  provider "aws" {
+    region = "us-west-1"
+  }
+
