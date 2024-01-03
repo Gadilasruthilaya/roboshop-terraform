46bfeac79855271cecf064088ed776bc1693bdb7
sruthilaya <sruthilayagadila@gmail.com>
2024-01-04 01:02:47 +0530

    vpc

--- main.tf
+++ main.tf
@@ -25,6 +25,6 @@ module "app"{
   tags = var.tags
   component = test
   subnet_id = lookup(lookup(lookup(lookup(module.vpc, "main" , null ), "subnet_ids", null), "app" , null), "subnet_ids",null)[0]
-  vpc_id = lookup(lookup(module.vpc, "main", null ), vpc_id , null)
+  vpc_id = lookup(lookup(module.vpc, "main", null ), "vpc_id" , null)
 }
 
