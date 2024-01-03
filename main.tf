32ffd418e291409d98a9b2c62a9cdc441bca591c
sruthilaya <sruthilayagadila@gmail.com>
2024-01-03 20:57:04 +0530

    vpc

--- main.tf
+++ main.tf
@@ -1,11 +1,17 @@
-module "instances" {
-  for_each  = var.component
-  source    = "git::https://github.com/Gadilasruthilaya/tf-module-app.git"
-  component = each.key
-  env       = var.env
-  tags      = merge(each.value["tags"], var.tags)
-}
+#module "instances" {
+#  for_each  = var.component
+#  source    = "git::https://github.com/Gadilasruthilaya/tf-module-app.git"
+#  component = each.key
+#  env       = var.env
+#  tags      = merge(each.value["tags"], var.tags)
+#}
   provider "aws" {
     region = "us-east-1"
   }
+
+module "vpc"{
+  source = "git::https://github.com/Gadilasruthilaya/tf-module-vpc.git"
+  for_each = var.vpc
+  cidr_block = each.value["cidr_block"]
+}
 
