888399d3defba210264ad42e9e7410f1913d4a99
sruthilaya <sruthilayagadila@gmail.com>
2024-01-05 13:53:46 +0530

    rabbitmq

--- variables.tf
+++ variables.tf
@@ -5,11 +5,7 @@ variable "tags"{
 }
 variable "vpc" {}
 variable "default_vpc_id" {}
-
-
 variable "default_route_id" {}
 variable "rabbitmq" {}
-
 variable "allow_ssh_cidr" {}
 variable "zone_id" {}
-variable "sg_subnet_cidr" {}
