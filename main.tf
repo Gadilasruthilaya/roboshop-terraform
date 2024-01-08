10429200c3b527518e586b8e5f27acad192b1977
sruthilaya <sruthilayagadila@gmail.com>
2024-01-08 22:50:08 +0530

    docdb

--- main.tf
+++ main.tf
@@ -62,41 +62,41 @@ module "rds"{
   skip_final_snapshot = each.value["skip_final_snapshot"]
 }
 
-module "elasticache" {
-  source = "git::https://github.com/Gadilasruthilaya/tf-module-elastic-cache.git"
-  for_each = var.elasticache
-  component = each.value["component"]
-  env = var.env
-  tags = var.tags
-  node_type                  = each.value["node_type"]
-  parameter_group_name       = each.value["parameter_group_name"]
-  num_node_groups            = each.value["num_node_groups"]
-  replicas_per_node_group    = each.value["replicas_per_node_group"]
-  engine                     = each.value["engine"]
-  engine_version             = each.value["engine_version"]
-  subnet_ids =  lookup(lookup(lookup(lookup(module.vpc, "main" , null ), "subnet_ids", null), "db" , null), "subnet_ids", null)
-  sg_subnet_cidr = lookup(lookup(lookup(lookup(var.vpc, "main" , null ), "subnets", null), "app" , null), "cidr_block", null)
-  vpc_id = lookup(lookup(module.vpc, "main", null ), "vpc_id" , null)
-  kms_key_arn = var.kms_key_arn
-}
-
-
-module "documentdb" {
-  source = "git::https://github.com/Gadilasruthilaya/tf-module-documentdb.git"
-  for_each = var.documentdb
-  component = each.value["component"]
-  env                        = var.env
-  tags                       = var.tags
-  engine                     = each.value["engine"]
-  engine_version             = each.value["engine_version"]
-  instance_count             = each.value["instance_count"]
-  instance_class             = each.value["instance_class"]
-  subnet_ids =  lookup(lookup(lookup(lookup(module.vpc, "main" , null ), "subnet_ids", null), "db" , null), "subnet_ids", null)
-  sg_subnet_cidr = lookup(lookup(lookup(lookup(var.vpc, "main" , null ), "subnets", null), "app" , null), "cidr_block", null)
-  vpc_id = lookup(lookup(module.vpc, "main", null ), "vpc_id" , null)
-  kms_key_arn = var.kms_key_arn
-
-}
+#module "elasticache" {
+#  source = "git::https://github.com/Gadilasruthilaya/tf-module-elastic-cache.git"
+#  for_each = var.elasticache
+#  component = each.value["component"]
+#  env = var.env
+#  tags = var.tags
+#  node_type                  = each.value["node_type"]
+#  parameter_group_name       = each.value["parameter_group_name"]
+#  num_node_groups            = each.value["num_node_groups"]
+#  replicas_per_node_group    = each.value["replicas_per_node_group"]
+#  engine                     = each.value["engine"]
+#  engine_version             = each.value["engine_version"]
+#  subnet_ids =  lookup(lookup(lookup(lookup(module.vpc, "main" , null ), "subnet_ids", null), "db" , null), "subnet_ids", null)
+#  sg_subnet_cidr = lookup(lookup(lookup(lookup(var.vpc, "main" , null ), "subnets", null), "app" , null), "cidr_block", null)
+#  vpc_id = lookup(lookup(module.vpc, "main", null ), "vpc_id" , null)
+#  kms_key_arn = var.kms_key_arn
+#}
+#
+#
+#module "documentdb" {
+#  source = "git::https://github.com/Gadilasruthilaya/tf-module-documentdb.git"
+#  for_each = var.documentdb
+#  component = each.value["component"]
+#  env                        = var.env
+#  tags                       = var.tags
+#  engine                     = each.value["engine"]
+#  engine_version             = each.value["engine_version"]
+#  instance_count             = each.value["instance_count"]
+#  instance_class             = each.value["instance_class"]
+#  subnet_ids =  lookup(lookup(lookup(lookup(module.vpc, "main" , null ), "subnet_ids", null), "db" , null), "subnet_ids", null)
+#  sg_subnet_cidr = lookup(lookup(lookup(lookup(var.vpc, "main" , null ), "subnets", null), "app" , null), "cidr_block", null)
+#  vpc_id = lookup(lookup(module.vpc, "main", null ), "vpc_id" , null)
+#  kms_key_arn = var.kms_key_arn
+#
+#}
 
 
 
